df5a7f0a79fe6b66197571855aad9aee833b9a11
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-09-23 16:00:02 +0100

    extract functions now run asynchronously

--- short-term-etl/extract.py
+++ short-term-etl/extract.py
@@ -1,44 +1,61 @@
 """Script with functions that extract the plant data from the API."""
 
-from requests import get
+import aiohttp
+import asyncio
 
 
-def request_plant(plant_id: int) -> dict:
-    """Request plant information from the API for a given plant id."""
+async def get_plant_data(session, url):
+    """Fetches the plant data from the API with the url provided."""
+    async with session.get(url) as response:
+        if response.status == 200:
+            content = await response.json()
+            return content
+        if response.status == 404:
+            # No plant found, so return None
+            return None
+        if response.status == 500:
+            return get_plant_data(url, session)
 
-    return get(f"https://sigma-labs-bot.herokuapp.com/api/plants/{plant_id}")
+
+async def get_batch_plant_data(start_id: int, end_id: int) -> list[dict]:
+    """Gets a batch of plant data between the provided plant ids."""
+
+    urls = [
+        f"https://sigma-labs-bot.herokuapp.com/api/plants/{i}" for i in range(start_id, end_id)]
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_plant_data(session, url) for url in urls]
+        responses = await asyncio.gather(*tasks)
+
+    return responses
 
 
-def extract_all_plant_data() -> list[dict]:
+def extract_all_plant_data(batch_size: int = 10) -> list[list]:
     """
-    Extract all plant data from the API, 
-    stopping after 10 consecutive missing plants.
+    Extracts all of the plant data with a chosen batch size.
+    The process will stop if a batch contains no plant data.
     """
 
-    searching = True
-    i = 1
-    count = 0
-    plants = []
+    plants_exist = True
+    i = 0
+    plant_data = []
 
-    while searching:
-        plant_req = request_plant(i)
+    while plants_exist:
+        start = batch_size*i + 1
+        end = batch_size*(i+1) + 1
 
-        if plant_req.status_code == 404:
-            i += 1
-            count += 1
-            if count == 10:
-                searching = False
+        plants = asyncio.run(get_batch_plant_data(start, end))
+
+        if not any(plants):
+            plants_exist = False
         else:
-            count = 0
-            while plant_req.status_code == 500:
-                plant_req = request_plant(i)
-
-            plants.append(plant_req.json())
+            plant_data.extend(plants)
             i += 1
 
-    return plants
+    return plant_data
 
 
-if __name__ == "__main__":
-    data = extract_all_plant_data()
-    print(data)
+def filter_plant_data(plant_data: list[dict]) -> list[dict]:
+    """Returns the plant data with any None values removed."""
+
+    return [p for p in plant_data if p]
