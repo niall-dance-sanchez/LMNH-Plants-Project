b2067f5ce188de0007e6a67978b3aa7c28804f8b
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-09-24 13:12:53 +0100

    tables entered in schema

---
+++ short-term-etl/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE delta.country (
+    country_id int IDENTITY(1,1) PRIMARY KEY,
+    country_name varchar
+)
+GO
+
+CREATE TABLE delta.city (
+    city_id int IDENTITY(1,1) PRIMARY KEY,
+    city_name varchar
+)
+GO
+
+CREATE TABLE delta.species (
+    species_id int IDENTITY(1,1) PRIMARY KEY,
+    species_name varchar
+)
+
+CREATE TABLE delta.botanist (
+    botanist_id int IDENTITY(1,1) PRIMARY KEY,
+    botanist_name, varchar
+    email varchar
+)
+GO
+
+CREATE TABLE delta.plant (
+    plant_id int PRIMARY KEY,
+    species_id int REFERENCES species, 
+    country_id int REFERENCES country,
+    city_id int REFERENCES city,
+    botanist_id int REFERENCES botanist
+)
+GO
+
+CREATE TABLE delta.reading (
+    reading_id int IDENTITY(1,1) PRIMARY KEY,
+    plant_id int REFERENCES plant,
+    temperature float,
+    soil_moisture float,
+    last_watered datetime2,
+    recording_taken datetime2
+)
+GO
