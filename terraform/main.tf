2b17ad3132bde46b5e7043f01c86a3d25cc2e59c
niall-dance-sanchez <niall0205@hotmail.co.uk>
2025-09-25 12:16:07 +0100

    terraform rds etl lambda, unsure about image URI however

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,3 +57,45 @@ resource "aws_glue_crawler" "c19_ajldka_glue_crawler_lmnh_plants" {
     path = "s3://${aws_s3_bucket.c19_ajldka_s3_lmnh_plants.bucket}"
   }
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "c19_ajldka_lambda_rds_etl_role_lmnh_plants" {
+  name               = "c19-ajldka-lambda-rds-etl-role-lmnh-plants"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_function" "c19_ajldka_lambda_function_lmnh_plants_rds_etl" {
+  function_name = "c19-ajldka-lambda-rds-etl"
+  role = aws_iam_role.example.arn
+  package_type = "Image"
+  image_uri = "" # TODO: Slightly confused about this, we need the image URI of 
+                  # the container image which we will have once it is launched, however how can 
+                  # have this as a fixed value if the image uri of the container is going to change if it is launched in the future? 
+  timeout = 300
+  memory_size = 1024
+  environment {
+    variables = {
+      AWS_ACCESS_KEY_AJLDKA = var.AWS_ACCESS_KEY
+      AWS_SECRET_ACCESS_KEY_AJLDKA = var.AWS_SECRET_ACCESS_KEY
+      DB_HOST=var.DB_HOST
+      DB_PORT=var.DB_PORT
+      DB_NAME=var.DB_NAME
+      DB_USER=var.DB_USER
+      DB_PASSWORD=var.DB_PASSWORD
+      DB_DRIVER=var.DB_DRIVER
+      DB_SCHEMA=var.DB_SCHEMA
+    }
+  }
+}
