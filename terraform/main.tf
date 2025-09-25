d8e719c60d79fbc7d28b7e859f8cc19f19274a02
yvonne <yvonnewong1107@gmail.com>
2025-09-25 15:21:01 +0100

    add iam role and policies and lambda function for long etl script

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,3 +107,47 @@ resource "aws_lambda_function" "c19_ajldka_lambda_function_lmnh_plants_rds_etl" 
     }
   }
 }
+
+resource "aws_iam_role" "c19_ajldka_lambda_s3_etl_role_lmnh_plants" {
+  name = "c19-ajldka-lambda-s3-etl-role-lmnh-plants"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "c19_ajldka_s3_etl_role_attach_lmnh_plants" {
+  role       = aws_iam_role.c19_ajldka_lambda_s3_etl_role_lmnh_plants.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "c19_ajldka_lambda_function_lmnh_plants_s3_etl" {
+  function_name = "c19-ajldka-lambda-s3-etl"
+  role = aws_iam_role.c19_ajldka_lambda_s3_etl_role_lmnh_plants.arn
+  package_type = "Image"
+  image_uri = "${aws_ecr_repository.c19_ajldka_ecr_lmnh_plants_s3.repository_url}/c19-ajldka-long-term-etl:latest" 
+  timeout = 300
+  memory_size = 1024
+  environment {
+    variables = {
+      AWS_ACCESS_KEY_AJLDKA = var.AWS_ACCESS_KEY
+      AWS_SECRET_ACCESS_KEY_AJLDKA = var.AWS_SECRET_ACCESS_KEY
+      DB_HOST=var.DB_HOST
+      DB_PORT=var.DB_PORT
+      DB_NAME=var.DB_NAME
+      DB_USER=var.DB_USER
+      DB_PASSWORD=var.DB_PASSWORD
+      DB_DRIVER=var.DB_DRIVER
+      DB_SCHEMA=var.DB_SCHEMA
+    }
+  }
+}
