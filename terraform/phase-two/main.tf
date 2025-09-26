8948d1127c0d4096a6f6015393e1acaced660945
yvonne <yvonnewong1107@gmail.com>
2025-09-26 15:26:10 +0100

    add ecs service, iam roles and task definition

--- terraform/phase-two/main.tf
+++ terraform/phase-two/main.tf
@@ -183,3 +183,153 @@ resource "aws_scheduler_schedule" "c19_ajldka_long_term_etl_scheduler" {
     role_arn = aws_iam_role.c19_ajldka_long_term_etl_scheduler_role.arn 
   }
 }
+
+# ecs task execution iam role
+resource "aws_iam_role" "c19_ajldka_task_execution_role" {
+  name = "c19-ajldka-task-execution-role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs-tasks.amazonaws.com"
+            }
+        }
+    ]
+})
+}
+
+# ecs task execution iam role policy
+resource "aws_iam_role_policy_attachment" "c19_ajldka_task_execution_policy" {
+  role= aws_iam_role.c19_ajldka_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ecs task definition
+resource "aws_ecs_task_definition" "c19_ajldka_ecs_plants_task_definition" {
+  family                   = "c19_ajldka_ecs_plants_task_definition"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn = aws_iam_role.c19_ajldka_task_execution_role.arn
+  task_role_arn = aws_iam_role.c19_ajldka_ecs_task_role.arn
+  container_definitions = jsonencode([{
+        name      = "c19-ajldka-plants-dashboard"
+        image     = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c19-ajldka-plants-dashboard:latest"
+        essential = true
+        portMappings = [
+        {
+          containerPort = 8501
+          hostPort      = 8501
+          protocol      = "tcp"
+        }
+        ],
+        environment = [
+        {name = "DB_HOST", value = var.DB_HOST},
+        {name = "DB_NAME", value = var.DB_NAME},
+        {name = "DB_USER", value = var.DB_USER},
+        {name = "DB_PASSWORD", value = var.DB_PASSWORD},
+        {name = "DB_PORT", value = var.DB_PORT},
+        {name = "DB_SCHEMA", value = var.DB_SCHEMA},
+        {name = "DB_DRIVER", value = var.DB_DRIVER},
+        {name = "AWS_ACCESS_KEY", value = var.AWS_ACCESS_KEY},
+        {name = "AWS_SECRET_ACCESS_KEY", value = var.AWS_SECRET_ACCESS_KEY},
+        {name = "AWS_REGION", value = var.AWS_REGION}
+        ],
+    logConfiguration = {
+        logDriver = "awslogs",
+        "options": {
+            awslogs-group = "/ecs/c19_ajldka_ecs_plants_task_definition"
+            awslogs-stream-prefix = "ecs"
+            awslogs-create-group = "true"
+            awslogs-region = "eu-west-2"
+        }
+    }
+    }
+  ])
+  runtime_platform {
+  operating_system_family = "LINUX"
+  cpu_architecture        = "X86_64"
+  }
+}
+
+##################################
+# ecs service task iam role
+resource "aws_iam_role" "c19_ajldka_ecs_task_role" {
+  name = "c19-ajldka-ecs-task-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "c19_ajldka_ecs_athena" {
+  role       = aws_iam_role.c19_ajldka_ecs_task_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "c19_ajldka_ecs_rds" {
+  role       = aws_iam_role.c19_ajldka_ecs_task_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
+# security group for ecs
+resource "aws_security_group" "c19_ajldka_sg" {
+  name        = "c19-ajldka-sg"
+  description = "Allow access to Streamlit dashboard"
+  vpc_id      = "vpc-0f29b6a6ab918bcd5"
+
+  ingress {
+    from_port   = 8501
+    to_port     = 8501
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ecs service
+resource "aws_ecs_service" "c19_ajldka_service" {
+  name            = "c19-ajldka-service"
+  cluster         = "arn:aws:ecs:eu-west-2:129033205317:cluster/c19-ecs-cluster"
+  task_definition = aws_ecs_task_definition.c19_ajldka_ecs_plants_task_definition.arn
+  desired_count   = "1"
+  network_configuration {
+    subnets          = ["subnet-00506a8db091bdf2a", "subnet-0425a4a0b929ea507", "subnet-0e7a1e60734c4fca7"]
+    security_groups  = [aws_security_group.c19_ajldka_sg.id]
+    assign_public_ip = true
+  }
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 100
+    base              = 1
+  }
+  deployment_circuit_breaker {
+    enable   = false
+    rollback = false
+  }
+
+  deployment_configuration {
+    bake_time_in_minutes = "0"
+    strategy             = "ROLLING"
+  }
+
+  deployment_controller {
+    type = "ECS"
+  }
+}
