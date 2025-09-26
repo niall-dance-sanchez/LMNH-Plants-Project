db69db332067c22ecb5ee79ddb4a8fe57209529e
Arshin Behjat <trainee.arshin.behjat@sigmalabs.co.uk>
2025-09-26 15:26:27 +0100

    Add tests for clean_plant_name

--- short-term-etl/test_transform.py
+++ short-term-etl/test_transform.py
@@ -2,7 +2,10 @@
 
 import pytest
 
-from transform import clean_plant_id
+from transform import (
+    clean_plant_id,
+    clean_plant_name
+)
 
 
 def test_clean_plant_id_correct_type_and_value(raw_data):
@@ -24,3 +27,16 @@ def test_clean_plant_id_incorrect_value(raw_data):
     with pytest.raises(ValueError) as exc_info:
         clean_plant_id(bad_data)
         assert "Invalid plant id" in exc_info.value
+
+
+def test_clean_plant_name_correct_type(raw_data):
+    plant_name = clean_plant_name(raw_data)
+    assert plant_name in raw_data["name"]
+
+
+def test_clean_plant_name_bad_type(raw_data):
+    bad_data = raw_data
+    bad_data["name"] = 101
+    with pytest.raises(ValueError) as exc_info:
+        clean_plant_name(bad_data)
+        assert "Invalid name type" in exc_info.value
