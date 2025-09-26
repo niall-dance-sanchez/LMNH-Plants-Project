7ea7de7ef8e03fad0fb58b6ad2deca0faed5e07e
yvonne <yvonnewong1107@gmail.com>
2025-09-26 09:29:28 +0100

    service lambda for iam role

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "c19-ajldka-terraform-state"
-#     key = "terraform.tfstate"
-#     region = var.AWS_REGION
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "c19-ajldka-terraform-state"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
