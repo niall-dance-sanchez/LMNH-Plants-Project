eca7c7bb3172becb41fe051e264d8cb7098200a5
Arshin Behjat <trainee.arshin.behjat@sigmalabs.co.uk>
2025-09-26 11:17:15 +0100

    Add updated tests for load_master_data.py

--- short-term-etl/test_load_master_data.py
+++ short-term-etl/test_load_master_data.py
@@ -1,19 +1,46 @@
 """Tests for functions created in the load_master_data file."""
 
-from load_master_data import (check_max_plant_id, has_new_data)
+from load_master_data import is_new_master_data
+
+incoming_data = [
+    {"data": "some data", "plant_id": 1},
+    {"data": "other data", "plant_id": 2},
+    {"data": "more data", "plant_id": 3},
+    {"data": "lots of data", "plant_id": 4},
+    {"data": "informative data", "plant_id": 5},
+    {"data": "dkhddsada", "plant_id": 6},
+    {"data": "hi", "plant_id": 7},
+    {"data": "done", "plant_id": 8}
+]
+existing_ids = [2, 3, 5, 8]
 
 
-def test_check_max_plant_id_returns_int():
-    """Test to check if max plant id function returns an integer value."""
-    id = check_max_plant_id([{"plant_id": 1}, {"plant_id": 2}, {"plant_id": 3}])
-    assert isinstance(id, int) == True
+def test_is_new_master_data_new_data():
+    """Test that only new master data is returned when present."""
+    new_data = is_new_master_data(incoming_data, existing_ids)
+    assert all(record["plant_id"] not in existing_ids for record in new_data)
 
 
-def test_check_has_new_data_returns_bool():
-    """Test to check if has new data function returns bool."""
-    assert has_new_data([{"plant_id": 1}, {"plant_id": 2}, {"plant_id": 3}, {"plant_id": 4}], [
-                        {"plant_id": 1}, {"plant_id": 2}, {"plant_id": 3}]) == True
-    assert has_new_data([{"plant_id": 1}, {"plant_id": 2}], [
-                        {"plant_id": 1}, {"plant_id": 2}, {"plant_id": 3}]) == False
+def test_is_new_master_data_new_data_matches():
+    """Assert that the new master data returned actually matches what was given."""
+    new_data = is_new_master_data(incoming_data, existing_ids)
+    assert all(record in incoming_data for record in new_data)
 
 
+def test_is_new_master_data_new_data_contains_dicts():
+    """Ensure the new data is returned as dictionaries."""
+    new_data = is_new_master_data(incoming_data, existing_ids)
+    assert all(isinstance(record, dict) for record in new_data)
+
+
+def test_is_new_master_data_new_data_is_list():
+    """Ensure new data is passed back as a list."""
+    new_data = is_new_master_data(incoming_data, existing_ids)
+    assert isinstance(new_data, list)
+
+
+def test_is_new_master_data_no_new_data():
+    """Test that new_data is None when there is no new master data."""
+    existing_ids = list(range(1, 9))
+    new_data = is_new_master_data(incoming_data, existing_ids)
+    assert new_data is None
