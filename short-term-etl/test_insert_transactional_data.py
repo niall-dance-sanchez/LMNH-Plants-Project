c5501a8912278a73bc790cb4a550f948426f0ba9
Arshin Behjat <trainee.arshin.behjat@sigmalabs.co.uk>
2025-09-26 15:09:23 +0100

    Change tests to use conftest

--- short-term-etl/test_insert_transactional_data.py
+++ short-term-etl/test_insert_transactional_data.py
@@ -1,60 +1,17 @@
+"""Tests for insert_transactional_data.py."""
+
 from insert_transactional_data import get_reading_tuples
 
-# TODO: Ideally this would be in a conftest.py, but since
-# we haven't got many tests yet I'll just keep it simple
-# and add it as a global. Should be removed when we have
-# a proper test suite and added to to conftest.py instead
-dummy_data = [
-    {
-        'plant_id': 8,
-        'name': 'Bird of paradise',
-        'temperature': 16.3483444707664,
-        'origin_location': {
-                'city': 'Edwardfurt',
-                'country': 'Liberia'
-        },
-        'botanist': {
-            'name': 'Bradford Mitchell Dvm',
-            'email': 'bradford.mitchell.dvm@lnhm.co.uk'
-        },
-        'last_watered': "2025-9-22 13:33:20",
-        'soil_moisture': 31.7511122641509,
-        'recording_taken': "2025-9-23 09:39:03"
-    },
-    {
-        'plant_id': 5,
-        'name': 'Flowery flower',
-        'temperature': 44.3483444707664,
-        'origin_location': {
-                'city': 'Edwardfurt',
-                'country': 'Liberia'
-        },
-        'botanist': {
-            'name': 'Bradford Mitchell Dvm',
-            'email': 'bradford.mitchell.dvm@lnhm.co.uk'
-        },
-        'last_watered': "2025-9-19 13:33:20",
-        'soil_moisture': 51.7511122641509,
-        'recording_taken': "2025-9-21 09:39:03"
-    }
-]
-readings = [
-    (8, 16.3483444707664, 31.7511122641509,
-     '2025-9-22 13:33:20', '2025-9-23 09:39:03'),
-    (5, 44.3483444707664, 51.7511122641509,
-     '2025-9-19 13:33:20', '2025-9-21 09:39:03')
-]
 
-
-def test_get_reading_tuples_regular_record_data():
+def test_get_reading_tuples_regular_record_data(cleaned_data, readings):
     """Just test readings are extract correctly from the dummy data."""
-    reading_tuples = get_reading_tuples(data=dummy_data)
+    reading_tuples = get_reading_tuples(data=cleaned_data)
     assert reading_tuples == readings
 
 
-def test_get_reading_tuples_singleton_data():
+def test_get_reading_tuples_singleton_data(cleaned_data, readings):
     """Test readings returned when data only has one record."""
-    singleton_data = [dummy_data[1]]
+    singleton_data = [cleaned_data[1]]
     reading_tuples = get_reading_tuples(data=singleton_data)
     assert reading_tuples == [readings[1]]
 
@@ -65,7 +22,7 @@ def test_get_reading_tuples_empty_data():
     assert len(reading_tuples) == 0
 
 
-def test_get_reading_tuples_all_tuples():
+def test_get_reading_tuples_all_tuples(cleaned_data):
     """Test all objects in the returned readings and make sure they're all tuples."""
-    reading_tuples = get_reading_tuples(data=dummy_data)
+    reading_tuples = get_reading_tuples(data=cleaned_data)
     assert all(isinstance(reading, tuple) for reading in reading_tuples)
--- short-term-etl/test_load_master_data.py
+++ short-term-etl/test_load_master_data.py
@@ -2,44 +2,32 @@
 
 from load_master_data import is_new_master_data
 
-incoming_data = [
-    {"data": "some data", "plant_id": 1},
-    {"data": "other data", "plant_id": 2},
-    {"data": "more data", "plant_id": 3},
-    {"data": "lots of data", "plant_id": 4},
-    {"data": "informative data", "plant_id": 5},
-    {"data": "dkhddsada", "plant_id": 6},
-    {"data": "hi", "plant_id": 7},
-    {"data": "done", "plant_id": 8}
-]
-existing_ids = [2, 3, 5, 8]
 
-
-def test_is_new_master_data_new_data():
+def test_is_new_master_data_new_data(incoming_data, existing_ids):
     """Test that only new master data is returned when present."""
     new_data = is_new_master_data(incoming_data, existing_ids)
     assert all(record["plant_id"] not in existing_ids for record in new_data)
 
 
-def test_is_new_master_data_new_data_matches():
+def test_is_new_master_data_new_data_matches(incoming_data, existing_ids):
     """Assert that the new master data returned actually matches what was given."""
     new_data = is_new_master_data(incoming_data, existing_ids)
     assert all(record in incoming_data for record in new_data)
 
 
-def test_is_new_master_data_new_data_contains_dicts():
+def test_is_new_master_data_new_data_contains_dicts(incoming_data, existing_ids):
     """Ensure the new data is returned as dictionaries."""
     new_data = is_new_master_data(incoming_data, existing_ids)
     assert all(isinstance(record, dict) for record in new_data)
 
 
-def test_is_new_master_data_new_data_is_list():
+def test_is_new_master_data_new_data_is_list(incoming_data, existing_ids):
     """Ensure new data is passed back as a list."""
     new_data = is_new_master_data(incoming_data, existing_ids)
     assert isinstance(new_data, list)
 
 
-def test_is_new_master_data_no_new_data():
+def test_is_new_master_data_no_new_data(incoming_data):
     """Test that new_data is None when there is no new master data."""
     all_ids = list(range(1, 9))
     new_data = is_new_master_data(incoming_data, all_ids)
